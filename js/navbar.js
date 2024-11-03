5acb96a26d6fb99f3883e0bc3e37c0a3d6351a9d
Tjpradeep <107289568+TjAthena@users.noreply.github.com>
2024-11-03 14:57:02 +0530

    testimonil content

--- js/navbar.js
+++ js/navbar.js
@@ -9,3 +9,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+function setLanguage(flagSrc, languageName) {
+    // Update the dropdown toggle to show the selected language flag and name
+    document.getElementById("activeLangFlag").src = flagSrc;
+    document.getElementById("activeLangFlag").alt = languageName;
+    document.getElementById("languageDropdown").innerHTML = 
+        '<img id="activeLangFlag" src="' + flagSrc + '" alt="' + languageName + '" width="20" class="me-2"> ' + languageName;
+}
